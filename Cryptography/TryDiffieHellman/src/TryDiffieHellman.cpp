b5e5c5279de74c3e08a3bc1ffe28c5ddca6c0c7d
MNandaK <m.nanda.k.07@gmail.com>
2021-12-25 23:03:27 +0700

    v0.7 TryDiffieHellman.cpp: Improve modulo exponential to have bigger key value

--- Cryptography/TryDiffieHellman/src/TryDiffieHellman.cpp
+++ Cryptography/TryDiffieHellman/src/TryDiffieHellman.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : TryDiffieHellman.cpp
 // Author      : Nandax
-// Version     : 0.5 - Minimal version only to get public key
+// Version     : 0.7 - Improve modulo exponential to have bigger key value
 // Copyright   : This is trial code, fell free to use with reference
 // Description : Try to implement basic simple shared key exchange DH algorithm
 //============================================================================
@@ -39,6 +39,23 @@ class Util
 
 			return result;
 		}
+
+		// This method calculate modulo of exponent integer,
+		// negative parameters will not working
+		unsigned long mod_exponent(unsigned short base, unsigned long exponent, unsigned short modulo)
+		{
+			unsigned long result = 1;
+
+			if(exponent != 0)
+				result = base;
+
+			//This formula is get from
+			//https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
+			for(unsigned short exp = 2; exp <= exponent; exp++)
+				result = ((result % modulo) * (base % modulo)) % modulo;
+
+			return result;
+		}
 };
 
 class DiffieHellman
@@ -49,7 +66,7 @@ class DiffieHellman
 		void setUpPrimitiveRootModulo()
 		{
 			//Below modulo numbers which primitive root is '2',
-			//was get from http://oeis.org/A001122/list
+			//is get from http://oeis.org/A001122/list
 			moduloChoose[0] = 19;
 			moduloChoose[1] = 227;
 			moduloChoose[2] = 797;
@@ -72,8 +89,8 @@ class DiffieHellman
 		{
 			Util util;
 
-			pubKeyA = util.pow_positive_int(primitiveRoot, secKeyA) % moduloChoose[chosenModulo-1];
-			pubKeyB = util.pow_positive_int(primitiveRoot, secKeyB) % moduloChoose[chosenModulo-1];
+			pubKeyA = util.mod_exponent(primitiveRoot, secKeyA, moduloChoose[chosenModulo-1]);
+			pubKeyB = util.mod_exponent(primitiveRoot, secKeyB, moduloChoose[chosenModulo-1]);
 		}
 
 		void setSecretKeyA(istream &in)
@@ -104,9 +121,10 @@ class DiffieHellman
 		}
 
 	private:
-		unsigned short secKeyA = 0;
-		unsigned short secKeyB = 0;
-		unsigned int pubKeyA, pubKeyB;
+		unsigned int secKeyA = 0;
+		unsigned int secKeyB = 0;
+		unsigned int pubKeyA = 1;
+		unsigned int pubKeyB = 1;
 		const unsigned short primitiveRoot = 2;
 		unsigned int moduloChoose[3];
 		short chosenModulo = 1;	//choose which number: 1->19, 2->227, 3->797
@@ -127,28 +145,14 @@ int main()
 	cout << "\n===========================================\n\n";
 
 	//Input secret key A
-	do {
-		cout << "Please input A secret key (0-63 value): ";
-		dh.setSecretKeyA(cin);
-
-		if(dh.getSecretKeyA() < 0 || dh.getSecretKeyA() > 63)
-			cout << "Key value is out of range, please input the correct value :)\n";
-
-		cout << "\n";
-
-	} while(dh.getSecretKeyA() < 0 || dh.getSecretKeyA() > 63);
+	cout << "Please input A secret key: ";
+	dh.setSecretKeyA(cin);
+	cout << "\n";
 
 	//Input secret key B
-	do {
-		cout << "Please input B secret key (0-63 value): ";
-		dh.setSecretKeyB(cin);
-
-		if(dh.getSecretKeyB() < 0 || dh.getSecretKeyB() > 63)
-			cout << "Key value is out of range, please input the correct value :)\n";
-
-		cout << "\n";
-
-	} while(dh.getSecretKeyB() < 0 || dh.getSecretKeyB() > 63);
+	cout << "Please input B secret key: ";
+	dh.setSecretKeyB(cin);
+	cout << "\n";
 
 	//Choose modulo
 	dh.setUpPrimitiveRootModulo();
@@ -163,8 +167,8 @@ int main()
 
 	cout << "\n\n======= Below values are to try and debug! ========\n\n";
 
-	cout << "\nTry: 2^20 = " << util.pow_positive_int(2, 20);
-	cout << "\nMax number in exponent = " << UINT_MAX;
+//	cout << "\nTry: 2^20 = " << util.pow_positive_int(2, 20);
+//	cout << "\nMax number in exponent = " << UINT_MAX;
 
 	unsigned int try1 = 4294967295;
 	unsigned int try2 = 4294967296;
@@ -186,13 +190,16 @@ int main()
 	unsigned long long tryLongLong = -1;
 	cout << "\nInt tryLongLong = " << tryLongLong << "\n";
 
+	unsigned short tryShort = -1;
+	cout << "\nInt tryShort = " << tryShort << "\n";
+
 //	unsigned __int128 try128_1 = -1;
 //	//unsigned __int128 try128_2 = 340282366920938463463374607431768211456;
 //
 //	//cout << "\nInt try128_1 = " << try128_1 << "\nInt try128_1 = " << try128_2 << "\n";
 //	cout << "\nInt try128_1 = " << to_string(try128_1) << "\n";
 
-	cout << "\nSecret key A = " << secKeyA << "\nSecret key B = " << secKeyB << "\n";
+//	cout << "\nSecret key A = " << secKeyA << "\nSecret key B = " << secKeyB << "\n";
 #endif
 
 	return 0;
