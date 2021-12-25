4b7540de855197b660237e9231b69da582e793ba
MNandaK <m.nanda.k.07@gmail.com>
2021-12-25 19:17:08 +0700

    Commit try Diffie-Hellman with Eclipse project setting

---
+++ Cryptography/TryDiffieHellman-protoUnused.cpp
@@ -0,0 +1,22 @@
+
+/******************************************************************************
+*
+* @file: TryDiffieHellman.cpp
+*
+* @description: Try to implement basic simple shared key exchange DH algorithm
+*
+* @author: Nandax
+*
+* @date: 211225
+*
+* @version: 0.0
+*
+******************************************************************************/
+
+int main()
+{
+	int 
+		
+	return 0;
+}
+
---
+++ Cryptography/TryDiffieHellman/Debug/src/TryDiffieHellman.d
@@ -0,0 +1,1 @@
+src/TryDiffieHellman.o: ../src/TryDiffieHellman.cpp
