7422130478e7f91b77c243c22475fe3f24f80986
Nandax <m.nanda.k@aim.com>
2021-12-25 11:26:18 +0700

    Add code for trial for implementing Diffie-Hellman algorithm

---
+++ Cryptography/TryDiffieHellman.cpp
@@ -0,0 +1,21 @@
+
+/******************************************************************************
+*
+* @file: TryDiffieHellman.cpp
+*
+* @description: Try to implement basic simple shared key exchange DH algorithm
+*
+* @author: Nandax
+*
+* @date: 211225
+*
+* @version: 0.0
+*
+******************************************************************************/
+
+int main()
+{
+	
+	return 0;
+}
+
